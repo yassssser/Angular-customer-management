1ab4b63fd9dbd125600eea5c482ea3ca0e0abaaa
Yasser Benmman <yasserbenmman@gmail.com>
2020-02-22 11:44:18 +0000

    add firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListClientComponent } from './components/list-client/list-client.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+import { PageNotFoundComponent } from './partials/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
+// import package firebase
+
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListClientComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBAJS_jE57mEuHB1MT1ZT3he-E_J2X432E",
+    authDomain: "gestion-client-92fe2.firebaseapp.com",
+    databaseURL: "https://gestion-client-92fe2.firebaseio.com",
+    projectId: "gestion-client-92fe2",
+    storageBucket: "gestion-client-92fe2.appspot.com",
+    messagingSenderId: "1059447924908",
+    appId: "1:1059447924908:web:71b89eed5f52fb9e329709"
+  }
 };
 
 /*
