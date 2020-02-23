70f34c22e8b5d777cb90c569c30c698c0e1a56b1
Yasser Benmman <yasserbenmman@gmail.com>
2020-02-23 14:20:24 +0000

    deploy on git

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,19 @@
 export const environment = {
   production: true,
-  firebase: {
+  firebase : {
+    apiKey: "AIzaSyBAJS_jE57mEuHB1MT1ZT3he-E_J2X432E",
+    authDomain: "gestion-client-92fe2.firebaseapp.com",
+    databaseURL: "https://gestion-client-92fe2.firebaseio.com",
+    projectId: "gestion-client-92fe2",
+    storageBucket: "gestion-client-92fe2.appspot.com",
+    messagingSenderId: "1059447924908",
+    appId: "1:1059447924908:web:71b89eed5f52fb9e329709"
+  }
+};
+
+
+/*
+firebase: {
     apiKey: "AIzaSyCcYIbc6Vb2KjzmcfOnfW7IRKzDivK3stk",
     authDomain: "gestion-client-d81d1.firebaseapp.com",
     databaseURL: "https://gestion-client-d81d1.firebaseio.com",
@@ -9,4 +22,4 @@ export const environment = {
     messagingSenderId: "776988168777",
     appId: "1:776988168777:web:355daa4f7fd5337f1a6598"
   }
-};
+*/
